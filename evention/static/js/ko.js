dcfd0075b6f254a3d490d1f18d511bca7fc7ce6f
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-03-23 17:32:07 -0400

    Fixed image cropping issue

--- evention/static/js/ko.js
+++ evention/static/js/ko.js
@@ -14,8 +14,6 @@ function BandSearchViewModel() {
         $.ajax({
             url: 'https://api.spotify.com/v1/search?q=' + encodeURI(newValue) + '&type=artist&limit=5',
             success: function (response) {
-                console.log(response);
-
                 for(var i=0; i < response.artists.items.length; i++) {
                     self.bandResults.push(new Band({name: response.artists.items[i].name,
                                                     image: response.artists.items[i].images[0].url}));

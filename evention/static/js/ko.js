95b6802b45fe649586aa77f9155ecaf68f5d3dd0
Eric Zaporzan <eric.zaporzan@gmail.com>
2016-03-23 17:06:37 -0400

    Searching bands now returns a list with images. Exciting stuff.

--- evention/static/js/ko.js
+++ evention/static/js/ko.js
@@ -1,18 +1,25 @@
-function Task(data) {
-    this.title = ko.observable(data.title);
-    this.isDone = ko.observable(data.isDone);
+function Band(data) {
+    this.name = ko.observable(data.name);
+    this.image = ko.observable(data.image);
 }
 
 function BandSearchViewModel() {
-    this.bandSearch = ko.observable('');
-    this.spotifySearch = ko.pureComputed(this.bandSearch).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 1000 } });
-
     var self = this;
+    self.bandSearch = ko.observable('');
+    self.spotifySearch = ko.pureComputed(this.bandSearch).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 1000 } });
+    self.bandResults = ko.observableArray([]);
+
     self.spotifySearch.subscribe(function(newValue) {
+        self.bandResults.removeAll();
         $.ajax({
-            url: 'https://api.spotify.com/v1/search?q=' + encodeURI(newValue) + '&type=artist&limit=3',
+            url: 'https://api.spotify.com/v1/search?q=' + encodeURI(newValue) + '&type=artist&limit=5',
             success: function (response) {
                 console.log(response);
+
+                for(var i=0; i < response.artists.items.length; i++) {
+                    self.bandResults.push(new Band({name: response.artists.items[i].name,
+                                                    image: response.artists.items[i].images[0].url}));
+                }
             }
         });
     });

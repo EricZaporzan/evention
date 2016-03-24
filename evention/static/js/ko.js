f5b933b7b5d865992d1e46c66a4151d1a0d07618
EricZaporzan <eric.zaporzan@gmail.com>
2016-03-23 23:35:11 -0400

    Adding favourite bands now works somewhat.

--- evention/static/js/ko.js
+++ evention/static/js/ko.js
@@ -1,6 +1,8 @@
 function Band(data) {
     this.name = ko.observable(data.name);
     this.image = ko.observable(data.image);
+    this.liked = ko.observable(data.liked);
+    this.disliked = ko.observable(!data.liked);
 }
 
 function BandSearchViewModel() {
@@ -17,16 +19,23 @@ function BandSearchViewModel() {
                 url: 'https://api.spotify.com/v1/search?q=' + encodeURI(newValue) + '&type=artist&limit=5',
                 success: function (response) {
                     for (var i = 0; i < response.artists.items.length; i++) {
-                        if (response.artists.items[i].images.length == 0) {
-                            image = "/static/images/noimage.png";
-                        }
-                        else {
+                        var image = "/static/images/noimage.png";
+                        if (response.artists.items[i].images.length > 0) {
                             image = response.artists.items[i].images[0].url;
                         }
 
+                        var liked = false;
+                        var favouriteBands = self.favouriteBands();
+                        for (var j = 0; j < favouriteBands.length; j++) {
+                            if (favouriteBands[j].name() == response.artists.items[i].name) {
+                                liked = true;
+                                break;
+                            }
+                        }
                         self.bandResults.push(new Band({
                             name: response.artists.items[i].name,
-                            image: image
+                            image: image,
+                            liked: liked
                         }));
                     }
                 }
@@ -34,17 +43,47 @@ function BandSearchViewModel() {
         }
     });
 
-    //self.getBands = function() {
-        $.ajax({
-            url: '/api/likes/',
+    $.ajax({
+        url: '/api/likes/',
+        success: function(response) {
+            for (var i=0; i < response.length; i++) {
+                self.favouriteBands.push(new Band({name: response[i].performer,
+                                                   image: response[i].image,
+                                                   liked: true}));
+            }
+        }
+    });
+
+    self.likeBand = function(item) {
+        user_url = $.ajax({
+            url: '/api/users/0/?format=json',
             success: function(response) {
-                for (var i=0; i < response.length; i++) {
-                    self.favouriteBands.push(new Band({name: response[i].performer, image: response[i].image}));
-                }
+                user_url = response.url;
+                $.ajax({
+                    method: 'POST',
+                    url: '/api/likes/',
+                    headers: {
+                        'X-CSRFToken': getCookie('csrftoken')
+                    },
+                    data: {
+                        owner: user_url,
+                        type: 'artist',
+                        performer: item.name,
+                        image: item.image
+                    },
+                    success: function(response) {
+                        console.log(response)
+                    },
+                    error: function(response) {
+                        console.log(response)
+                    }
+                });
             }
         });
+        console.log(user_url);
 
-    //}
+
+    }
 }
 
 ko.applyBindings(new BandSearchViewModel());
--- evention/static/js/project.js
+++ evention/static/js/project.js
@@ -1,1 +1,6 @@
 /* Project specific Javascript goes here. */
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}

8783284cceb13603147e109917f870e2beaa2660
EricZaporzan <eric.zaporzan@gmail.com>
2016-03-25 19:12:07 -0400

    Removed some unneeded imports.

--- evention/events/views.py
+++ evention/events/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework import filters
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
 
 from .models import Likes
 from .serializers import LikesSerializer

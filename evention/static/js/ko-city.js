9fde05ee1ff4a05d9700efecc5ba9702f68addb3
EricZaporzan <eric.zaporzan@gmail.com>
2016-04-25 20:28:52 -0400

    Moved back to the Google API. More relevant results returned, faster = good.

--- evention/static/js/ko-city.js
+++ evention/static/js/ko-city.js
@@ -1,6 +1,6 @@
 function City(data) {
     this.likeId = ko.observable(data.likeId);
-    this.code = ko.observable(data.code);
+    this.placeId = ko.observable(data.placeId);
     this.city = ko.observable(data.city);
     this.region = ko.observable(data.region);
     this.country = ko.observable(data.country);
@@ -13,57 +13,80 @@ function City(data) {
 function CitySearchViewModel() {
     var self = this;
     self.citySearch = ko.observable('');
-    self.geobytesSearch = ko.pureComputed(this.citySearch).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 750 } });
+    self.googleSearch = ko.pureComputed(this.citySearch).extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 500 } });
     self.cityResults = ko.observableArray([]);
     self.favouriteCities = ko.observableArray([]);
 
-    self.geobytesSearch.subscribe(function(newValue) {
+    self.googleSearch.subscribe(function(newValue) {
         self.cityResults.removeAll();
         if (newValue != "") {
-            jQuery.getJSON(
-                "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+newValue,
-                function (data) {
-                    var totalDisplayed = 0;
-                    for (var i = 0; i < Math.min(10, data.length); i++) {
-                        jQuery.getJSON(
-                            "http://gd.geobytes.com/GetCityDetails?callback=?&fqcn="+data[i],
-                            function (detail) {
-                                var favouriteCities = self.favouriteCities();
-                                var alreadyLiked = false;
-                                var likeId = -1;
-
-                                for (var j = 0; j < favouriteCities.length; j++) {
-                                    if (favouriteCities[j].code() == detail.geobyteslocationcode) {
-                                        if (favouriteCities[j].liked()) {
-                                            alreadyLiked = true;
-                                            break;
-                                        }
-                                        else {
-                                            likeId = favouriteCities[j].likeId();
-                                            break;
-                                        }
-                                    }
-                                }
-                                if (!alreadyLiked) {
-                                    self.cityResults.push(new City({
-                                        likeId: likeId,
-                                        code: detail.geobyteslocationcode,
-                                        city: detail.geobytescity,
-                                        region: detail.geobytesregion,
-                                        country: detail.geobytescountry,
-                                        latitude: detail.geobyteslatitude,
-                                        longitude: detail.geobyteslongitude,
-                                        liked: false
-                                    }));
-                                }
-                            }
-                        );
-                    }
-
-                }
-            );
+            var service = new google.maps.places.AutocompleteService();
+            service.getQueryPredictions({ input: newValue, options: {type: 'cities'}}, self.queryCallback);
         }
     });
+
+    self.queryCallback = function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].types && results[i].types[0] == 'locality') {
+                    var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({placeId: results[i].place_id}, function(geoResult, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            var lat = geoResult[0].geometry.location.lat();
+                            var lng = geoResult[0].geometry.location.lng();
+
+                            var alreadyLiked = false;
+                            var likeId = -1;
+                            var favouriteCities = self.favouriteCities();
+
+                            for (var j = 0; j < favouriteCities.length; j++) {
+                                if (favouriteCities[j].placeId() == geoResult[0].place_id) {
+                                    if (favouriteCities[j].liked()) {
+                                        alreadyLiked = true;
+                                        break;
+                                    }
+                                    else {
+                                        likeId = favouriteCities[j].likeId();
+                                        break;
+                                    }
+                                }
+                            }
+
+                            var city = '';
+                            var region = '';
+                            var country = '';
+
+                            for (var k = 0; k < geoResult[0].address_components.length; k++) {
+                                if (geoResult[0].address_components[k].types[0] == "locality") {
+                                    city = geoResult[0].address_components[k].long_name;
+                                }
+                                if (geoResult[0].address_components[k].types[0] == "administrative_area_level_1") {
+                                    region = geoResult[0].address_components[k].long_name;
+                                }
+                                if (geoResult[0].address_components[k].types[0] == "country") {
+                                    country = geoResult[0].address_components[k].long_name;
+                                }
+                            }
+
+                            if (!alreadyLiked) {
+                                self.cityResults.push(new City({
+                                    likeId: likeId,
+                                    placeId: geoResult[0].place_id,
+                                    city: city,
+                                    region: region,
+                                    country: country,
+                                    latitude: lat,
+                                    longitude: lng,
+                                    liked: false
+                                }));
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    };
+
 
     $.ajax({
         method: 'GET',
@@ -71,7 +94,7 @@ function CitySearchViewModel() {
         success: function(response) {
             for (var i=0; i < response.length; i++) {
                 self.favouriteCities.push(new City({likeId: response[i].id,
-                                                    code: response[i].city.code,
+                                                    placeId: response[i].city.code,
                                                     city: response[i].city.city,
                                                     region: response[i].city.region,
                                                     country: response[i].city.country,
@@ -94,7 +117,7 @@ function CitySearchViewModel() {
             data: {
                 owner: username,
                 id: item.likeId(),
-                code: item.code(),
+                code: item.placeId(),
                 city: item.city(),
                 region: item.region(),
                 country: item.country(),
@@ -126,7 +149,7 @@ function CitySearchViewModel() {
             data: {
                 owner: username,
                 id: item.likeId(),
-                code: item.code(),
+                code: item.placeId(),
                 city: item.city(),
                 region: item.region(),
                 country: item.country(),

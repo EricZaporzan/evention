b244d4cb1405917f950fc273a1903c2bf90cdda7
EricZaporzan <eric.zaporzan@gmail.com>
2016-04-25 20:35:09 -0400

    Added some bindings to nudge the user along to like cities.

--- evention/static/js/ko-city.js
+++ evention/static/js/ko-city.js
@@ -169,7 +169,43 @@ function CitySearchViewModel() {
     };
 }
 
+ko.bindingHandlers.visibleElements = {
+    update: function(element, valueAccessor, allBindings) {
+        va = ko.utils.unwrapObservable(valueAccessor());
+        var show = false;
+        for (var i = 0; i < va.length; i++) {
+            if (va[i].liked()) {
+                show = true;
+                break;
+            }
+        }
+        if(show) {
+            $(element).css('display', '');
+        }
+        else {
+            $(element).css('display', 'none');
+        }
+    }
+};
 
+ko.bindingHandlers.noVisibleElements = {
+    update: function(element, valueAccessor, allBindings) {
+        va = ko.utils.unwrapObservable(valueAccessor());
+        var show = false;
+        for (var i = 0; i < va.length; i++) {
+            if (va[i].liked()) {
+                show = true;
+                break;
+            }
+        }
+        if(show) {
+            $(element).css('display', 'none');
+        }
+        else {
+            $(element).css('display', '');
+        }
+    }
+};
 
 
 ko.applyBindings(new CitySearchViewModel());
